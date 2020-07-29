9c312c72d4e8b4dc4dd576154f54689ab3812790
Roman Belotsvetov <rbelotsvetov88@yandex.ru>
2020-07-29 10:53:30 +0300

    ...

--- src/componentes/App/index.js
+++ src/componentes/App/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Api from '../../api';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUSD, getEUR } from '../../store/actions';
@@ -12,6 +12,8 @@ const App = () => {
     const dispatchUSD = useDispatch()
     const dispatchEUR = useDispatch()
 
+    const [selected, setSelected] = useState({});
+
     useEffect(() => {
         Api.getCurrency().then((data) => {
             return data.Valute
@@ -24,10 +26,23 @@ const App = () => {
 
     const USD = useSelector(state => state.currencyReducer.USD);
     const EUR = useSelector(state => state.currencyReducer.EUR);
+
+    useEffect(() => {
+        if(USD) {
+            setSelected(USD)
+        }
+    }, [USD])
     
     return <div className={'main-frame'}>
-        <Form {...USD}/>
-        <Form {...EUR}/>
+        <div>
+            {selected.Value}
+        </div>
+        <div onClick={() => setSelected(USD)}>
+            <Form {...USD}/>
+        </div>
+        <div onClick={() => setSelected(EUR)}>
+            <Form {...EUR}/>
+        </div>
     </div>
 }
 
--- src/componentes/Form/index.js
+++ src/componentes/Form/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loading } from '../Loader';
 
 import './style.scss';
 
 export const Form = valute => {
 
+    const [value, setValue] = useState('');
+
+    useEffect(() => {
+        if (valute) {
+            setValue(valute.Nominal)
+        }
+    }, [valute])
+
     if (!valute) {
         return <Loading/>
+    }
+
+    const handleChange = e => {
+        let value = +e.target.value
+        if ((/\d+/g).test(value)) {
+            setValue(value)
+        }
     }
     
     return (
         <form>
-            <input/>
+            <input value={value} onChange={handleChange} type='text'/>
             <button>{valute.CharCode}</button>
             <span>{valute.Name}</span>
             {valute.CharCode === 'USD' ? ' \u0024' : ' \u20AC'}
